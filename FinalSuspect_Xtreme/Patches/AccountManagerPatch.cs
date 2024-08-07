97b99c7814303d836940898b33d0f621945b226c
Slok7565 <Slok7565@qq.com>
2024-08-07 17:08:45 +0800

    .

--- FinalSuspect_Xtreme/Patches/AccountManagerPatch.cs
+++ FinalSuspect_Xtreme/Patches/AccountManagerPatch.cs
@@ -44,7 +44,6 @@ public static class UpdateFriendCodeUIPatch
         {
             newRequest.transform.localPosition -= new Vector3(0f, 0f, 10f);
             newRequest.transform.localScale = new Vector3(0.8f, 1f, 1f);
-            
         }
 
         if (GameHeader = GameObject.Find("BarSprite"))
