209e567c200b41503ed153509c5da72e1d8f091b
Slok7565 <Slok7565@qq.com>
2024-12-01 12:34:29 +0800

    Bug fixes

---
+++ FinalSuspect/Patches/EndGameManagerPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace FinalSuspect;
+
+[HarmonyPatch]
+public class EndGameManagerPatch
+{
+    [HarmonyPatch(typeof(EndGameManager), nameof(EndGameManager.ShowButtons)), HarmonyPostfix]
+    public static void ShowButtons_Postfix(EndGameManager __instance)
+    {
+        if (!Main.AutoEndGame.Value) return;
+        new LateTask(__instance.Navigation.NextGame, 2f, "Auto End Game");
+    }
+}
