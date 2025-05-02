c2422782a8191c33e390d8309b5ec09e46d0a39e
QingFeng <qingfeng_awa@163.com>
2025-05-03 02:43:48 +0800

    恢复API

--- FinalSuspect/Modules/Resources/VersionChecker.cs
+++ FinalSuspect/Modules/Resources/VersionChecker.cs
@@ -94,7 +94,7 @@ public static class VersionChecker
             Info("File MD5: " + md5, "CheckRelease");
             Info("Github Url: " + downloadUrl_github, "CheckRelease");
             Info("Gitee Url: " + downloadUrl_gitee, "CheckRelease");
-            //Info("Website Url: " + downloadUrl_xtremeapi, "CheckRelease");
+            Info("Website Url: " + downloadUrl_xtremeapi, "CheckRelease");
 
             if (firstLaunch || isBroken)
             {
