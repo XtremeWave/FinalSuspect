c5f8a0796a6147c4079c241ccf01d09bb1bf78bd
NewLezaiYa <3651440375@qq.com>
2025-09-10 13:26:18 +0800

    反作弊更新

---
+++ FinalSuspect/DataHandling/FinalAntiCheat/Handlers/Valid/VanishHandler.cs
@@ -0,0 +1,29 @@
+using AmongUs.GameOptions;
+using FinalSuspect.DataHandling.FinalAntiCheat.Interfaces;
+using FinalSuspect.Modules.Core.Game.PlayerControlExtension;
+using Hazel;
+
+namespace FinalSuspect.DataHandling.FinalAntiCheat.Handlers.Valid;
+
+public class VanishHandler : IRpcHandler
+{
+    public List<byte> TargetRpcs =>
+    [
+        (byte)RpcCalls.CheckVanish,
+        (byte)RpcCalls.StartVanish,
+        (byte)RpcCalls.CheckAppear,
+        (byte)RpcCalls.StartAppear,
+    ];
+
+    public bool HandleLobby(PlayerControl sender, MessageReader reader,
+        ref bool notify, ref string reason, ref bool ban)
+    {
+        return true;
+    }
+
+    public bool HandleGame_All(PlayerControl sender, MessageReader reader,
+        ref bool notify, ref string reason, ref bool ban)
+    {
+        return sender.GetRoleType() is not RoleTypes.Viper and not RoleTypes.ImpostorGhost;
+    }
+}
