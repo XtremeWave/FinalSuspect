8cb8e7d2ceccf2d9f1624322777e4a0b33613e92
NewLezaiYa <3651440375@qq.com>
2025-10-04 02:04:48 +0800

    VersionCheck RPC

---
+++ FinalSuspect/DataHandling/FinalAntiCheat/Handlers/Invalid/VersionCheck.cs
@@ -0,0 +1,19 @@
+using FinalSuspect.DataHandling.FinalAntiCheat.Interfaces;
+using Hazel;
+
+namespace FinalSuspect.DataHandling.FinalAntiCheat.Handlers.Invalid;
+
+public class VersionCheck : IRpcHandler
+{
+    public List<byte> TargetRpcs =>
+    [
+        80
+    ];
+
+    public bool HandleInvalidRPC(PlayerControl sender, MessageReader reader,
+        ref bool notify, ref string reason, ref bool ban)
+    {
+        reason = "VersionCheck";
+        return false;
+    }
+}
