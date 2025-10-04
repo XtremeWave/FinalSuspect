50d1a8c4c9abfff574fa0f7ce728208458b516e8
Slok7565 <Slok7565@qq.com>
2025-10-04 23:11:18 +0800

    Support Finish

--- FinalSuspect/Patches/System/CreditsControllerPatch.cs
+++ FinalSuspect/Patches/System/CreditsControllerPatch.cs
@@ -10,7 +10,7 @@ public class CreditsControllerPatch
         var devList = new List<string>
         {
             $"<size=120%><color={ColorHelper.FSColorHex}>{Main.ModName}</color></size>",
-            $"<color=#fffcbe>By</color> <color={ColorHelper.AuthorColorHex}>By Slok</color>",
+            $"<color=#fffcbe>By</color> <color={ColorHelper.AuthorColorHex}>Slok</color>",
             "<color=#fffcbe>Thanks:</color> <color=#ffff00>LezaiYa</color>",
             //Others
             $"<size=120%>{GetString("Id.Contributor")}</size>",
@@ -78,7 +78,8 @@ public class CreditsControllerPatch
     public static void AddCreditPrefix(CreditsController __instance,
         [HarmonyArgument(0)] CreditsController.CreditStruct originalCredit)
     {
-        if (originalCredit.columns[0] != "logoImage") return;
+        //这里安卓还有点小问题
+        if (originalCredit?.columns[0] != "logoImage") return;
 
         foreach (var credit in GetModCredits())
         {
