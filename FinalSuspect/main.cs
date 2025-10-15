ed33c4ec6a1b15c9c104c9fc3ca3afc031676a84
NewLezaiYa <3651440375@qq.com>
2025-10-15 09:45:04 +0800

    更改nuget和main里的版本号

--- FinalSuspect/main.cs
+++ FinalSuspect/main.cs
@@ -30,7 +30,7 @@ public class Main : BasePlugin
     public const int PluginCreation = 1;
 
     // == 版本相关设定 / Version Config ==
-    public const string LowestSupportedVersion = "2025.9.9"; // 17.0.0
+    public const string LowestSupportedVersion = "2025.10.14"; // 17.0.1
 
     private const string DisplayedVersion_Head = "1.3";
 
