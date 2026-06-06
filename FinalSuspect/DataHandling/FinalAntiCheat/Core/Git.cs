975718d2938c2fe02135598075ee7aed84eaabd9
NewLezaiYa <3651440375@qq.com>
2026-06-06 23:28:11 +0800

    同步2.0的git改版

--- FinalSuspect/DataHandling/FinalAntiCheat/Core/Git.cs
+++ FinalSuspect/DataHandling/FinalAntiCheat/Core/Git.cs
@@ -1,25 +1,56 @@
 #if Windows
-using System.IO;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 #endif
+
+namespace FinalSuspect.DataHandling.FinalAntiCheat.Core;
 
 [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
 // ReSharper disable once CheckNamespace
-internal static class Git
+internal static class ExeChecker
 {
+#if Windows
+    private static System.Threading.Timer _windowCheckTimer;
+    private static bool _isQuitting;
+#endif
+
     public static void Prefix(MainMenuManager __instance)
     {
 #if Windows
-        return;
-        // 获取当前Dll启动目录
-        var directoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        // 针对基于BepInEx注入检测
-        if (directoryPath == null) return;
-        foreach (var path in Directory.EnumerateFiles(directoryPath, "*.*"))
+        // 检测非法进程
+        Process[] procs = Process.GetProcessesByName("AmongUsCosmetics");
+        if (procs.Length > 0)
         {
-            var fileName = Path.GetFileName(path);
-
-            if (fileName is not "FinalSuspect.dll" and not "PolarNight.dll") Application.Quit(1);
+            Error($"检测到非法进程: AmongUsCosmetics.exe！游戏将被强制终止。", "FAC");
+            Application.Quit(1);
         }
+
+        // 检测非法窗口标题
+        IntPtr hWnd = FindWindow(null, "HackerHansen's Among Us Cosmetics Unlocker for v16.1.0s (updated 6/15/2025)");
+        if (hWnd != IntPtr.Zero)
+        {
+            Error($"检测到非法进程: AmongUsCosmetics.exe！游戏将被强制终止。", "FAC");
+            Application.Quit(1);
+        }
+
+        // 每1分钟检测一次非法窗口标题
+        _windowCheckTimer ??= new System.Threading.Timer(_ =>
+            {
+                if (_isQuitting) return;
+                IntPtr hWndTimer = FindWindow(null, "HackerHansen's Among Us Cosmetics Unlocker for v16.1.0s (updated 6/15/2025)");
+                if (hWndTimer != IntPtr.Zero)
+                {
+                    _isQuitting = true;
+                    Error($"检测到非法进程: AmongUsCosmetics.exe！游戏将被强制终止。", "FAC");
+                    Application.Quit(1);
+                }
+            }, null, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
 #endif
     }
+
+#if Windows
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+#endif
 }
