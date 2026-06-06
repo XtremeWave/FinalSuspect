1873073312c599faf50f9658909dbf6e4cafe466
NewLezaiYa <3651440375@qq.com>
2026-06-06 23:38:12 +0800

    网站域名更新

--- FinalSuspect/Modules/Resources/PathManager.cs
+++ FinalSuspect/Modules/Resources/PathManager.cs
@@ -67,7 +67,7 @@ public static class PathManager
 
     public const string QQInviteUrl = "https://qm.qq.com/q/ZA7Lnjz3SC";
     public const string DiscordInviteUrl = "https://discord.gg/kz787Zg7h8/";
-    public const string WebsiteUrl = "https://finalsuspect.pages.dev/";
+    public const string WebsiteUrl = "https://finalsuspect.zh-cn.edgeone.cool/";
     public const string GithubRepoUrl = "https://github.com/Slok7565/FinalSuspect/";
 
     public const string BugReportUrl_Github =
