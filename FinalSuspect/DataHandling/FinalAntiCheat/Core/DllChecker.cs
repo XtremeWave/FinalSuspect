8ab9dfe247aa4d65d77bce55659c6f33a47f0508
Slok <137697902+Slok7565@users.noreply.github.com>
2026-07-29 21:50:37 +0800

    Revert "Encryption" (#41)

--- FinalSuspect/DataHandling/FinalAntiCheat/Core/DllChecker.cs
+++ FinalSuspect/DataHandling/FinalAntiCheat/Core/DllChecker.cs
@@ -10,10 +10,6 @@ namespace FinalSuspect.DataHandling.FinalAntiCheat.Core;
 
 public static class DllChecker
 {
-#if Windows
-    private static readonly Type LinkedGuard = typeof(ExeChecker);
-#endif
-
     [PluginModuleInitializer(InitializePriority.High)]
     internal static void OnInitialization()
     {
--- FinalSuspect/DataHandling/FinalAntiCheat/Core/ExeChecker.cs
+++ FinalSuspect/DataHandling/FinalAntiCheat/Core/ExeChecker.cs
@@ -13,7 +13,6 @@ internal static class ExeChecker
 #if Windows
     private static System.Threading.Timer _windowCheckTimer;
     private static bool _isQuitting;
-    private static readonly Type LinkedGuard = typeof(DllChecker);
 #endif
 
     public static void Prefix(MainMenuManager __instance)
